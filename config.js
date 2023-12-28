70d1286d4b0d05f5549e44e23f2fd14ff57e5aba
abhimalu0 <155104323+abhimalu0@users.noreply.github.com>
2023-12-29 04:08:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['9567813275', 'LATRIN BROTHERS', true],
+  ['9526423865', 'ABHI', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.botname = 'LATRIN BROTHERS_MD'
+global.premium = 'false'
+global.packname = 'abhi┃ᴮᴼᵀ' 
+global.author = '@abhiabhijith' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
